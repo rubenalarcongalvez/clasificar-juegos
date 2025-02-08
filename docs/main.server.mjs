2b0e73096e8e10c9ee35bac8ad5926ca616be4b7
rubenalarcongalvez <rubenalarcongalvez@gmail.com>
2025-02-08 19:49:29 +0100

    Corregido tooltip

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CZN5CFSL.mjs";import"./chunk-6TYBEMPV.mjs";import"./chunk-WUI6SWGE.mjs";export{a as default};
+import{a}from"./chunk-LG4FGYUZ.mjs";import"./chunk-6TYBEMPV.mjs";import"./chunk-WUI6SWGE.mjs";export{a as default};
